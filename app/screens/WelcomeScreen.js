9752515f67278ef69931e51b14999e280355b3a0
Mushaib Khan <mushaibkhan@gmail.com>
2022-02-22 03:29:57 +0530

    new test line for git pull

--- app/screens/WelcomeScreen.js
+++ app/screens/WelcomeScreen.js
@@ -37,6 +37,7 @@ const styles = StyleSheet.create({
     logo: {
         width: 100,
         height: 100,
+        
     },
     logoContainer: {
         position: "absolute",
@@ -47,3 +48,5 @@ const styles = StyleSheet.create({
 })
 
 export default WelcomeScreen;
+
+// test
