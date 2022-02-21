e72761e98cb7324fc9dfbe873c4feda07a3da905
Mushaib Khan <mushaibkhan@gmail.com>
2022-02-21 21:25:39 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,7 @@
+import { StatusBar } from 'expo-status-bar';
+import { View } from 'react-native';
+import WelcomeScreen from './app/screens/WelcomeScreen';
+
+export default function App() {
+  return    <WelcomeScreen />;
+  }
